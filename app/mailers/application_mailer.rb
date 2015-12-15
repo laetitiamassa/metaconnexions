61a63932cb69d931ed3193151a2011e21e3290a1
Laetitia Massa <laetitiamassa@gmail.com>
2015-12-15 19:21:05 +0100

    changed bg header color

---
+++ app/mailers/application_mailer.rb
@@ -0,0 +1,4 @@
+class ApplicationMailer < ActionMailer::Base
+  default from: "contact@metaconnexions.com"
+  layout 'mailer'
+end
---
+++ app/mailers/user_mailer.rb
@@ -0,0 +1,15 @@
+class UserMailer < ApplicationMailer
+  default from: 'contact@metaconnexions.com'
+ 
+  def contact_sender(user)
+    @user = user
+    @url  = 'http://example.com/login'
+    mail(to: @user.email, subject: 'Welcome to My Awesome Site')
+  end
+
+  def contact_receiver(user)
+    @user = user
+    @url  = 'http://example.com/login'
+    mail(to: @user.email, subject: 'Welcome to My Awesome Site')
+  end
+end
